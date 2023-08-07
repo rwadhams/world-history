d02dd8d12a7c707cdb78a86644e01158fee58590
robert.wadhams <robert.wadhams@gmail.com>
2023-08-07 13:54:34 +1000

    cleanup

--- WorldHistoryApplication/src/main/groovy/com/wadhams/world/history/app/WorldHistoryReportingApp.groovy
+++ WorldHistoryApplication/src/main/groovy/com/wadhams/world/history/app/WorldHistoryReportingApp.groovy
@@ -1,18 +1,8 @@
 package com.wadhams.world.history.app
 
-import java.text.NumberFormat
-import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.time.temporal.ChronoUnit
 import com.wadhams.world.history.controller.WorldHistoryController
-import groovy.transform.ToString
 
 class WorldHistoryReportingApp {
-	DateTimeFormatter xmlDTF = DateTimeFormatter.ofPattern('dd/MM/yyyy')
-	DateTimeFormatter reportingDTF = DateTimeFormatter.ofPattern('EEE dd/MM/yyyy')
-
 	static main(args) {
 		println 'WorldHistoryReportingApp started...'
 		println ''
